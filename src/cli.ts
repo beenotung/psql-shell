e3ed143f9e4dc309980d1b550f5705ed2cb46254
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-06-11 01:42:27 +0800

    show table schema

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,9 @@
 import pg from 'pg'
 import { config } from 'dotenv'
 import readline, { Interface } from 'readline'
+import Knex, { Knex as KnexType } from 'knex'
+import { scanPGTableSchema } from 'quick-erd/dist/db/pg-to-text'
+import { tableToString } from 'quick-erd/dist/core/table'
 
 async function main() {
   let io: Interface = readline.createInterface({
@@ -10,6 +13,7 @@ async function main() {
     output: process.stdout,
   })
   let client: pg.Client | undefined
+  let knex: KnexType | undefined
 
   function ask(query: string) {
     return new Promise<string>((resolve, reject) => {
@@ -60,8 +64,32 @@ async function main() {
     client = connection.client
     let { database, user, password, host, port } = connection
 
+    async function querySingleColumn(sql: string) {
+      let result = await client!.query(sql)
+      let field = result.fields[0].name
+      console.log(result.rows.map(row => row[field]).join(', '))
+    }
+
+    function getKnex() {
+      if (!knex) {
+        knex = Knex({
+          client: 'pg',
+          connection: {
+            database,
+            user,
+            password,
+            host,
+            port,
+          },
+        })
+      }
+      return knex
+    }
+
     for (;;) {
       let text = await ask(`${database}=# `)
+      if (!text.trim()) continue
+      if (text.startsWith('\\q')) break
       if (text.startsWith('\\c')) {
         let db = text.match(/\\c ([\w-_]+)/)?.[1]
         if (db) {
@@ -75,12 +103,35 @@ async function main() {
         )
         continue
       }
+      if (text.startsWith('\\l')) {
+        await querySingleColumn(/* sql */ `select datname from pg_database`)
+        continue
+      }
+      if (text.startsWith('\\d')) {
+        let tableName = text.replace('\\d', '').replace(';', '').trim()
+        if (tableName) {
+          let tables = await scanPGTableSchema(getKnex())
+          let table = tables.find(table => table.name === tableName)
+          if (!table) {
+            console.log(`Did not find any relation named "${tableName}".`)
+            continue
+          }
+          console.log(tableToString(table).trim())
+        } else {
+          await querySingleColumn(
+            /* sql */ `select tablename from pg_tables where schemaname = 'public'`,
+          )
+        }
+        continue
+      }
+      console.log('unknown command:', text)
     }
   } catch (error) {
     console.error(error)
   } finally {
     io.close()
     client?.end()
+    knex?.destroy()
   }
 }
 main()
